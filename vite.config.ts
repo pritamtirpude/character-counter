08789240ed7d633a7924bf778728ea18051f4781
Pritam tirpude <pritam.tirpude9601@gmail.com>
2025-03-05 22:49:22 +0530

    test setup is implemented using vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 import { VitePWA } from 'vite-plugin-pwa';
@@ -124,5 +124,10 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src')
     }
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: '/src/tests/setupTests.ts'
   }
 });
